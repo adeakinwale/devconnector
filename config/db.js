b17d61d1be9ecd8955e65a2618e2cbf18a52c312
Admin <you@example.com>
2019-11-17 06:58:09 +0100

    user registration

--- config/db.js
+++ config/db.js
@@ -4,11 +4,16 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, { useNewUrlParser: true });
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
-    // EXIT process with failure
+    // Exit process with failure
     process.exit(1);
   }
 };
